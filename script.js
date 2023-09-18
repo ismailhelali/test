dfa77014f655c3f6520528149383899315fbbaaa
ismailhelali <40539850+ismailhelali@users.noreply.github.com>
2023-09-18 17:48:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-// Fetch the product data from the external file "products.js"
-fetch('products.js')
-  .then(response => response.json())
-  .then(products => {
+// Import the product data from the external file "products.js"
+// Make sure products.js is in the same directory as this script
+import { products } from './products.js';
+
+// Function to populate the table with product data
+function populateTable() {
     const productTable = document.getElementById('productTable');
-    const tbody = productTable.getElementsByTagName('tbody')[0];
+    const tbody = document.getElementById('tableBody');
 
     // Loop through the products and add rows to the table
     products.forEach(product => {
-      const row = document.createElement('tr');
-      row.innerHTML = `
-        <td>${product.date}</td>
-        <td>${product.type}</td>
-        <td>${product.client}</td>
-        <td>${product.name}</td>
-        <td>${product.quantity}</td>
-      `;
-      tbody.appendChild(row);
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${product.date}</td>
+            <td>${product.type}</td>
+            <td>${product.client}</td>
+            <td>${product.name}</td>
+            <td>${product.quantity}</td>
+        `;
+        tbody.appendChild(row);
     });
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+}
+
+// Call the populateTable function when the page loads
+window.addEventListener('load', populateTable);
+
