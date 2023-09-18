3a6428fc83ccb78b7e4633e01456d0388bad1c89
ismailhelali <40539850+ismailhelali@users.noreply.github.com>
2023-09-18 16:26:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,88 +1,40 @@
-// Get the client name from local storage
-let clientName = localStorage.getItem('clientName');
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+  const searchInput = document.getElementById('search-input');
+  const productList = document.getElementById('product-list');
 
-if (!clientName) {
-    clientName = prompt('Please enter your name or the name of your company:');
-    localStorage.setItem('clientName', clientName);
-}
+  // Function to filter and display products
+  function displayFilteredProducts(query) {
+    const filteredProducts = products.filter(product =>
+      product.name.toLowerCase().includes(query.toLowerCase()) ||
+      product.client.toLowerCase().includes(query.toLowerCase())
+    );
 
-// Display the client name in a stylish div
-const nameDiv = document.createElement('div');
-nameDiv.innerHTML = `Client Name: ${clientName}`;
-nameDiv.classList.add('client-name');
-document.body.appendChild(nameDiv);
+    productList.innerHTML = ''; // Clear existing product list
 
-function generateTableRows(products) {
-    const table = document.getElementById('product-table');
-    table.innerHTML = ''; // clear the table
-    products.forEach(product => {
-        const row = document.createElement('tr');
-        row.className = "active-row";
-        row.innerHTML = `
-            <td>${product.date}</td>
-            <td>${product.type}</td>
-            <td>${product.client}</td>
-            <td>${product.name}</td>
-            <td>
-                <input class="inputquantity" type="number" value="${product.quantity}">
-            </td>
+    if (filteredProducts.length === 0) {
+      productList.innerHTML = '<p>No products found.</p>';
+    } else {
+      filteredProducts.forEach(product => {
+        const productDiv = document.createElement('div');
+        productDiv.classList.add('product');
+        productDiv.innerHTML = `
+          <h2>${product.name}</h2>
+          <p>Date: ${product.date}</p>
+          <p>Type: ${product.type}</p>
+          <p>Client: ${product.client}</p>
+          <p>Quantity: ${product.quantity}</p>
         `;
+        productList.appendChild(productDiv);
+      });
+    }
+  }
 
-        const quantityInput = row.querySelector('input[type="number"]');
-        quantityInput.addEventListener('change', () => {
-            product.quantity = parseInt(quantityInput.value);
-        });
+  // Event listener for search input
+  searchInput.addEventListener('input', function () {
+    displayFilteredProducts(searchInput.value);
+  });
 
-        table.appendChild(row);
-    });
-}
-
-const searchField = document.getElementById('search-field');
-const categorySelect = document.getElementById('category-select');
-
-searchField.addEventListener('input', () => {
-    const query = searchField.value.toLowerCase();
-    const filteredProducts = products.filter(product => {
-        return product.name.toLowerCase().includes(query) || product.category.toLowerCase().includes(query);
-    });
-    generateTableRows(filteredProducts);
+  // Initial display of all products
+  displayFilteredProducts('');
 });
-
-categorySelect.addEventListener('change', () => {
-    const category = categorySelect.value;
-    const filteredProducts = category ? products.filter(product => product.category === category) : products;
-    generateTableRows(filteredProducts);
-});
-
-window.onload = () => {
-    generateTableRows(products);
-};
-
-// Add your additional JavaScript code here
-
-function generatePdf() {
-    let p = [];
-    let i = 1;
-    products.forEach(product => {
-        if (product.checked == true) {
-            p.push([i++, product.name, product.category, 0, product.quantity, product.unit, 0]);
-        }
-    });
-
-    props.invoice.table = p;
-
-    var pdfObject = jsPDFInvoiceTemplate.default(props);
-    console.log('Object created:', pdfObject);
-}
-
-var props = {
-    outputType: jsPDFInvoiceTemplate.OutputType.Save,
-    returnJsPDFDocObject: true,
-    fileName: "solidernet 2023",
-    orientationLandscape: false,
-    compress: true,
-
-    // Add your invoice and business details here
-
-    // ... (rest of the properties)
-};
