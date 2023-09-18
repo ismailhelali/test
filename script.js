d345f494b033f8ab6eff25bc2b5bcf32797e8fbc
ismailhelali <40539850+ismailhelali@users.noreply.github.com>
2023-09-18 17:30:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,23 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function () {
-  const searchInput = document.getElementById('search-input');
-  const productList = document.getElementById('product-list');
+// Fetch the product data from the external file "products.js"
+fetch('products.js')
+  .then(response => response.json())
+  .then(products => {
+    const productTable = document.getElementById('productTable');
+    const tbody = productTable.getElementsByTagName('tbody')[0];
 
-  // Function to filter and display products
-  function displayFilteredProducts(query) {
-    const filteredProducts = products.filter(product =>
-      product.name.toLowerCase().includes(query.toLowerCase()) ||
-      product.client.toLowerCase().includes(query.toLowerCase())
-    );
-
-    productList.innerHTML = ''; // Clear existing product list
-
-    if (filteredProducts.length === 0) {
-      productList.innerHTML = '<p>No products found.</p>';
-    } else {
-      filteredProducts.forEach(product => {
-        const productDiv = document.createElement('div');
-        productDiv.classList.add('product');
-        productDiv.innerHTML = `
-          <h2>${product.name}</h2>
-          <p>Date: ${product.date}</p>
-          <p>Type: ${product.type}</p>
-          <p>Client: ${product.client}</p>
-          <p>Quantity: ${product.quantity}</p>
-        `;
-        productList.appendChild(productDiv);
-      });
-    }
-  }
-
-  // Event listener for search input
-  searchInput.addEventListener('input', function () {
-    displayFilteredProducts(searchInput.value);
+    // Loop through the products and add rows to the table
+    products.forEach(product => {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td>${product.date}</td>
+        <td>${product.type}</td>
+        <td>${product.client}</td>
+        <td>${product.name}</td>
+        <td>${product.quantity}</td>
+      `;
+      tbody.appendChild(row);
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
   });
-
-  // Initial display of all products
-  displayFilteredProducts('');
-});
