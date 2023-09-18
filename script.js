856ed40e5a214f6c2926095bad79b43579065a24
ismailhelali <40539850+ismailhelali@users.noreply.github.com>
2023-09-18 17:50:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-// Import the product data from the external file "products.js"
-// Make sure products.js is in the same directory as this script
-import { products } from './products.js';
-
 // Function to populate the table with product data
 function populateTable() {
     const productTable = document.getElementById('productTable');
     const tbody = document.getElementById('tableBody');
+
+    // Define your product data in an array
+    const products = [
+        {
+            date: '08/16/2023',
+            type: 'LC',
+            client: '7TH GARDEN  ICE:003051951000084',
+            name: 'Vinaigre blanc PIK 50 cl',
+            quantity: 26
+        },
+        {
+            date: '08/16/2023',
+            type: 'LC',
+            client: '7TH GARDEN  ICE:003051951000084',
+            name: 'Serviette ARAX 30 x 30',
+            quantity: 100
+        },
+        // Add more product entries here
+    ];
 
     // Loop through the products and add rows to the table
     products.forEach(product => {
@@ -23,4 +38,3 @@ function populateTable() {
 
 // Call the populateTable function when the page loads
 window.addEventListener('load', populateTable);
-
